c458058d5ce4bfff819c4a9708ea901cd862489c
elizapimentel <elizapimentel@hotmail.com>
2024-07-24 17:33:37 -0300

    refact: add serializable and fix mappedBy name

--- spaceprobe/src/main/java/com/br/eliza/spaceprobe/model/Planet.java
+++ spaceprobe/src/main/java/com/br/eliza/spaceprobe/model/Planet.java
@@ -1,10 +1,12 @@
 package com.br.eliza.spaceprobe.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,15 +14,18 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Planet {
-    @Id
+public class Planet implements Serializable {
+
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty("id")
+    @Id
     private Long planetId;
+
     private String planetName;
     private int width;
     private int height;
 
-    @OneToMany(mappedBy = "planets", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "planet", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Rover> rovers = new ArrayList<>();
 
     public void addRover(Rover rover) {
