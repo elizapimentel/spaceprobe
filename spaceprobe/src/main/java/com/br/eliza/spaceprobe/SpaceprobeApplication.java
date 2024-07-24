7aa153a19675126a2a2eb2398add158da5a6e730
elizapimentel <elizapimentel@hotmail.com>
2024-07-24 11:07:18 -0300

    first commit

---
+++ spaceprobe/src/main/java/com/br/eliza/spaceprobe/SpaceprobeApplication.java
@@ -0,0 +1,13 @@
+package com.br.eliza.spaceprobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceprobeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceprobeApplication.class, args);
+	}
+
+}
---
+++ spaceprobe/src/test/java/com/br/eliza/spaceprobe/SpaceprobeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.eliza.spaceprobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceprobeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
