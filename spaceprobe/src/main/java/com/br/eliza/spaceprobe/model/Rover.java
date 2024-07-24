5688a62e8f0207b3a11f9baf1ce45a11136760c2
elizapimentel <elizapimentel@hotmail.com>
2024-07-24 13:40:13 -0300

    refact: delete methods

--- spaceprobe/src/main/java/com/br/eliza/spaceprobe/model/Rover.java
+++ spaceprobe/src/main/java/com/br/eliza/spaceprobe/model/Rover.java
@@ -28,54 +28,6 @@ public class Rover {
     @JoinColumn(name = "planet_id")
     private Planet planet;
 
-    public void processCommands(List<Character> commands) {
-        for (char command : commands) {
-            switch (command) {
-                case 'M':
-                    move();
-                    break;
-                case 'L':
-                    turnLeft();
-                    break;
-                case 'R':
-                    turnRight();
-                    break;
-                default:
-                    throw new IllegalArgumentException("Invalid command: " + command);
-            }
-        }
-    }
 
-    private void move() {
-        switch (direction) {
-            case NORTH:
-                if (coordinates.getY() + 1 < planet.getHeight()) {
-                    coordinates.setY(coordinates.getY() + 1);
-                }
-                break;
-            case SOUTH:
-                if (coordinates.getY() - 1 >= 0) {
-                    coordinates.setY(coordinates.getY() - 1);
-                }
-                break;
-            case EAST:
-                if (coordinates.getX() + 1 < planet.getWidth()) {
-                    coordinates.setX(coordinates.getX() + 1);
-                }
-                break;
-            case WEST:
-                if (coordinates.getX() - 1 >= 0) {
-                    coordinates.setX(coordinates.getX() - 1);
-                }
-                break;
-        }
-    }
 
-    private void turnLeft() {
-        direction = direction.turnLeft();
-    }
-
-    private void turnRight() {
-        direction = direction.turnRight();
-    }
 }
