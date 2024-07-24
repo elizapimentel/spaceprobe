072925608a2968a0bfdce5dd7c0059b29bbd547f
elizapimentel <elizapimentel@hotmail.com>
2024-07-24 13:59:40 -0300

    feat: add repositories

--- spaceprobe/src/main/java/com/br/eliza/spaceprobe/repository/PlanetRepository.java
+++ spaceprobe/src/main/java/com/br/eliza/spaceprobe/repository/PlanetRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PlanetRepo extends JpaRepository<Planet, Long> {
+public interface PlanetRepository extends JpaRepository<Planet, Long> {
 }
--- spaceprobe/src/main/java/com/br/eliza/spaceprobe/repository/RoverRepository.java
+++ spaceprobe/src/main/java/com/br/eliza/spaceprobe/repository/RoverRepository.java
@@ -1,2 +1,9 @@
-package com.br.eliza.spaceprobe.repository;public interface RoverRepository {
+package com.br.eliza.spaceprobe.repository;
+
+import com.br.eliza.spaceprobe.model.Rover;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoverRepository extends JpaRepository<Rover, Long> {
 }
