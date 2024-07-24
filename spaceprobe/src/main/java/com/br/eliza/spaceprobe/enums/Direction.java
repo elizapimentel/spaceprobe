82fb4771288548b8168435cb790198d88cdb4075
elizapimentel <elizapimentel@hotmail.com>
2024-07-24 13:11:17 -0300

    feat: add enum direction

--- spaceprobe/src/main/java/com/br/eliza/spaceprobe/enums/Direction.java
+++ spaceprobe/src/main/java/com/br/eliza/spaceprobe/enums/Direction.java
@@ -1,2 +1,25 @@
-package com.br.eliza.spaceprobe.enums;public enum Direction {
+package com.br.eliza.spaceprobe.enums;
+
+public enum Direction {
+    NORTH, EAST, SOUTH, WEST;
+
+    public Direction turnLeft() {
+        switch (this) {
+            case NORTH: return WEST;
+            case EAST: return NORTH;
+            case SOUTH: return EAST;
+            case WEST: return SOUTH;
+        }
+        throw new IllegalArgumentException("Invalid direction: " + this);
+    }
+
+    public Direction turnRight() {
+        switch (this) {
+            case NORTH: return EAST;
+            case EAST: return SOUTH;
+            case SOUTH: return WEST;
+            case WEST: return NORTH;
+        }
+        throw new IllegalArgumentException("Invalid direction: " + this);
+    }
 }
