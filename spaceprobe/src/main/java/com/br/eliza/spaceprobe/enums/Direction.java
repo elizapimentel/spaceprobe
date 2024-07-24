d1b9598e26a64063fcc3ff2b6650968a911b66ee
elizapimentel <elizapimentel@hotmail.com>
2024-07-24 13:52:03 -0300

    refact: change methods to service

--- spaceprobe/src/main/java/com/br/eliza/spaceprobe/enums/Direction.java
+++ spaceprobe/src/main/java/com/br/eliza/spaceprobe/enums/Direction.java
@@ -2,24 +2,4 @@ package com.br.eliza.spaceprobe.enums;
 
 public enum Direction {
     NORTH, EAST, SOUTH, WEST;
-
-    public Direction turnLeft() {
-        switch (this) {
-            case NORTH: return WEST;
-            case EAST: return NORTH;
-            case SOUTH: return EAST;
-            case WEST: return SOUTH;
-        }
-        throw new IllegalArgumentException("Invalid direction: " + this);
-    }
-
-    public Direction turnRight() {
-        switch (this) {
-            case NORTH: return EAST;
-            case EAST: return SOUTH;
-            case SOUTH: return WEST;
-            case WEST: return NORTH;
-        }
-        throw new IllegalArgumentException("Invalid direction: " + this);
-    }
 }
