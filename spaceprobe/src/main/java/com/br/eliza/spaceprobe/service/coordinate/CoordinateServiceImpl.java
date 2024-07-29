003d5c8bd2392a7885243c6b52fbc80bf6c2ee76
elizapimentel <elizapimentel@hotmail.com>
2024-07-29 16:22:59 -0300

    refact: add handler

--- spaceprobe/src/main/java/com/br/eliza/spaceprobe/service/coordinate/CoordinateServiceImpl.java
+++ spaceprobe/src/main/java/com/br/eliza/spaceprobe/service/coordinate/CoordinateServiceImpl.java
@@ -1,6 +1,7 @@
 package com.br.eliza.spaceprobe.service.coordinate;
 
 import com.br.eliza.spaceprobe.enums.Direction;
+import com.br.eliza.spaceprobe.exceptions.InvalidCommandException;
 import com.br.eliza.spaceprobe.model.Coordinates;
 import com.br.eliza.spaceprobe.model.Planet;
 import org.springframework.stereotype.Service;
@@ -15,37 +16,37 @@ public class CoordinateServiceImpl implements CoordinateService{
     }
 
     @Override
-    public Coordinates calculateNewCoordinates(Coordinates currentCoordinates,
-                                               Direction direction) {
+        public Coordinates calculateNewCoordinates(Coordinates currentCoordinates,
+                                                   Direction direction) {
 
-        if (currentCoordinates == null || direction == null) {
-            throw new IllegalArgumentException("Coordinates and directions must not be null.");
+            if (currentCoordinates == null || direction == null) {
+                throw new InvalidCommandException("Coordinates and directions must not be null.");
+            }
+
+            int x = currentCoordinates.getX();
+            int y = currentCoordinates.getY();
+
+            if (x <= 0 || y <= 0) {
+                throw new InvalidCommandException("Coordinates must not be 0 nor negative.");
+            }
+
+            switch (direction) {
+                case NORTH:
+                    y++;
+                    break;
+                case SOUTH:
+                    y--;
+                    break;
+                case EAST:
+                    x++;
+                    break;
+                case WEST:
+                    x--;
+                    break;
+            }
+
+            return new Coordinates(x, y);
         }
-
-        int x = currentCoordinates.getX();
-        int y = currentCoordinates.getY();
-
-        if (x < 0 || y < 0) {
-            throw new IllegalArgumentException("Coordinates must be non-negative.");
-        }
-
-        switch (direction) {
-            case NORTH:
-                y++;
-                break;
-            case SOUTH:
-                y--;
-                break;
-            case EAST:
-                x++;
-                break;
-            case WEST:
-                x--;
-                break;
-        }
-
-        return new Coordinates(x, y);
-    }
 
     @Override
     public Direction turnLeft(Direction direction) {
