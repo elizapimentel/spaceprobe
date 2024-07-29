bd9883cf610d82a3186879e137ddf24208bdfb72
elizapimentel <elizapimentel@hotmail.com>
2024-07-29 16:37:26 -0300

    feat: add spring validation

--- spaceprobe/src/main/java/com/br/eliza/spaceprobe/controller/rover/RoverController.java
+++ spaceprobe/src/main/java/com/br/eliza/spaceprobe/controller/rover/RoverController.java
@@ -6,12 +6,14 @@ import com.br.eliza.spaceprobe.service.rover.RoverService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/rovers")
+@Validated
 public class RoverController {
 
     private final RoverService service;
