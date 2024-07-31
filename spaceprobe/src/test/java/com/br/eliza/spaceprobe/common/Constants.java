d147d62a474b1487a9de6271e862ea741352f746
elizapimentel <elizapimentel@hotmail.com>
2024-07-31 18:07:24 -0300

    feat: add more constants

--- spaceprobe/src/test/java/com/br/eliza/spaceprobe/common/Constants.java
+++ spaceprobe/src/test/java/com/br/eliza/spaceprobe/common/Constants.java
@@ -17,11 +17,17 @@ public class Constants {
     public static final Long ROVER_ID                  = 1L;
     public static final int X                          = 1;
     public static final int Y                          = 2;
+    public static final int NEGATIVE_X                 = -1;
+    public static final int NEGATIVE_Y                 = -1;
     public static final Direction DIRECTION            = Direction.NORTH;
     public static final boolean IS_ON                  = true;
     public static final Coordinates COORDINATES        = new Coordinates(X, Y);
 
     public static final String PLANET_BASE_URL               = "/v2/planets";
     public static final String ROVER_BASE_URL                = "/v2/rovers";
+
+    public static final String INVALID_COORDINATES_MESSAGE = "Coordinates must not be negative.";
+    public static final String PLANET_NOT_FOUND_MESSAGE = "Planet not found";
+    public static final String ROVER_NOT_FOUND_MESSAGE = "Rover not found";
 
 }
