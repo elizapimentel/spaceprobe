5ba97fef986eb02ca40550c1f52496f70c29f476
elizapimentel <elizapimentel@hotmail.com>
2024-08-01 20:42:52 -0300

    test: update Constants

--- spaceprobe/src/test/java/com/br/eliza/spaceprobe/common/Constants.java
+++ spaceprobe/src/test/java/com/br/eliza/spaceprobe/common/Constants.java
@@ -12,22 +12,20 @@ public class Constants {
     public static final int HEIGHT                     = 5;
 
 
-
     //rover
     public static final Long ROVER_ID                  = 1L;
     public static final int X                          = 1;
     public static final int Y                          = 2;
-    public static final int NEGATIVE_X                 = -1;
-    public static final int NEGATIVE_Y                 = -1;
     public static final Direction DIRECTION            = Direction.NORTH;
-    public static final boolean IS_ON                  = true;
+    public static final boolean IS_ON                  = false;
     public static final Coordinates COORDINATES        = new Coordinates(X, Y);
+
 
     public static final String PLANET_BASE_URL               = "/v2/planets";
     public static final String ROVER_BASE_URL                = "/v2/rovers";
 
-    public static final String INVALID_COORDINATES_MESSAGE = "Coordinates must not be negative.";
     public static final String PLANET_NOT_FOUND_MESSAGE = "Planet not found";
-    public static final String ROVER_NOT_FOUND_MESSAGE = "Rover not found";
+    public static final String ROVER_ID_NOT_FOUND_MESSAGE = "Rover not found with ID: ";
+    public static final String COORDINATE_ALREADY_OCCUPIED_MESSAGE = "Coordinate is already occupied on the new planet. Try to relocate the rover first.";
 
 }
