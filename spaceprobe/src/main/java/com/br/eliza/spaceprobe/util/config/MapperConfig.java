c0c9101a716ae482ffc2e5ec762065570a802128
elizapimentel <elizapimentel@hotmail.com>
2024-08-02 16:29:39 -0300

    chore: move to util

--- spaceprobe/src/main/java/com/br/eliza/spaceprobe/util/config/MapperConfig.java
+++ spaceprobe/src/main/java/com/br/eliza/spaceprobe/util/config/MapperConfig.java
@@ -1,11 +1,11 @@
-package com.br.eliza.spaceprobe.config;
+package com.br.eliza.spaceprobe.util.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AppConfig {
+public class MapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
